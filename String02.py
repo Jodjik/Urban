4939ebea860a41fbe3feec329f1460910360e919
Jodjik <jodjik@mail.ru>
2024-07-02 20:31:18 +0300

    String02.py

--- String02.py
+++ String02.py
@@ -1,7 +1,7 @@
-name="это моя строка"
-print(name[0])
-print(name[-1])
-print(name[2:5])
-print(name[::-1])
-print(len(name))
-print("это новая строка"+ name)
+word="это моя строка"
+print(word[0])
+print(word[-1])
+print(word[2:5])
+print(word[::-1])
+print(len(word))
+print("это новая строка"+ word)
