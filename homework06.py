8fa8691112b950cb109d09927f1d443922ebb2f8
Jodjik <jodjik@mail.ru>
2024-07-09 20:49:52 +0300

    Add files via upload

---
+++ homework06.py
@@ -0,0 +1,12 @@
+my_dict = {'Ilya': 25, 'Roma': 28, 'Jenya': 28, 'Olga': 55}
+print(my_dict)
+print(my_dict.get('Ilya'))
+print(my_dict.get('Oleg'))
+my_dict.update({'Ivan': 26,
+                'Anton': 32})
+print(my_dict)
+my_set = {True, 'Anton', 5, 5, True, 55, 5}
+print(my_set)
+my_set.update({False, "Bus"})
+my_set.remove(5)
+print(my_set)
