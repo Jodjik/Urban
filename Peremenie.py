f199eb41628ff4c0a5ea032ac1ca09474737015c
Jodjik <jodjik@mail.ru>
2024-11-26 20:34:10 +0300

    Add files via upload

---
+++ Peremenie.py
@@ -0,0 +1,5 @@
+done_hw = 12
+hours_count = 1.5
+class_name = 'Python'
+time_for_hw = hours_count/done_hw
+print(f'{class_name}, всего задач: {done_hw}, затрачено часов: {hours_count}, среднее время выполнения {time_for_hw} часа.')
