4948e4afc687d106810c7dba3475c5640885558a
Jodjik <jodjik@mail.ru>
2025-01-17 09:32:47 +0300

    Add files via upload

---
+++ Fake_math.py
@@ -0,0 +1,6 @@
+def divide(x,y):
+    if y == 0:
+       return 'Ошибка!'
+    else:
+        d = x/y
+        return d
---
+++ True_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide(x,y):
+    if y == 0:
+        return inf
+    else:
+        d = x/y
+        return d
